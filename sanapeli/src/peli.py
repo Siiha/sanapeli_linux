42ccc23b4dce137bad765d67a3e93ada4b53f68d
Siiha <90766306+Siiha@users.noreply.github.com>
2021-12-10 20:45:50 +0200

    Add files via upload

--- sanapeli/src/peli.py
+++ sanapeli/src/peli.py
@@ -1,7 +1,9 @@
 import random,tkinter,sys,os,getpass,spui
 os.chdir('/home/'+getpass.getuser()+'/sanapeli/src')
-base = tkinter.Tk()
+base = spui.pohja()
+base.geometry("400x250")
 base.title("Sanapeli")
 seuraava = tkinter.Button(base, text="seuraava",command=spui.seuraava)
 seuraava.pack()
+
 base.mainloop()
--- sanapeli/src/spui.py
+++ sanapeli/src/spui.py
@@ -1,37 +1,43 @@
 import kysymykset as ksms
 import tkinter,random
 from functools import partial
+def pohja():
+	return tkinter.Tk()
 def seuraava():
-	viesti = tkinter.Label(text="Valitse haluamasi kysymys tyyppi.")
+	base = pohja()
+	viesti = tkinter.Label(base,text="Valitse haluamasi kysymys tyyppi.")
 	viesti.pack()
-	ekaVika = tkinter.Button(text="eka tai vika kirjain",command=eka_vika )
+	ekaVika = tkinter.Button(base,text="eka tai vika kirjain",command=eka_vika )
 	ekaVika.pack()
-	pituus1 = tkinter.Button(text="Tietyn pituinen sana",command=pituus)
+	pituus1 = tkinter.Button(base,text="Tietyn pituinen sana",command=pituus)
 	pituus1.pack()
 def tarkistus(x,y):
+	base = pohja()
 	x = x.get()
 	if x in y[0]:
-		viesti = tkinter.Label(text="Oikein!")
+		viesti = tkinter.Label(base,text="Oikein!")
 	else:
-		viesti = tkinter.Label(text="Väärin!")
+		viesti = tkinter.Label(base,text="Väärin!")
 	viesti.pack()
 def eka_vika():
+	base = pohja()
 	k1 = ksms.ekavika(ksms.k[random.randint(0,len(ksms.k)-1)],random.randint(0,1))
-	viesti = tkinter.Label(text=k1[1])
+	viesti = tkinter.Label(base,text=k1[1])
 	viesti.pack()
-	syote = tkinter.Entry()
+	syote = tkinter.Entry(base)
 	syote.pack()
-	valmis=tkinter.Button(text="valmis",command=partial(tarkistus,syote,k1))
+	valmis=tkinter.Button(base,text="valmis",command=partial(tarkistus,syote,k1))
 	valmis.pack()
 	
 	
  
 def pituus():
+	base = pohja()
 	k1 = ksms.pitka(random.randint(3,30))
-	viesti = tkinter.Label(text=k1[1])
+	viesti = tkinter.Label(base,text=k1[1])
 	viesti.pack()
-	syote = tkinter.Entry()
+	syote = tkinter.Entry(base)
 	syote.pack()
-	valmis=tkinter.Button(text="valmis",command=partial(tarkistus,syote,k1))
+	valmis=tkinter.Button(base,text="valmis",command=partial(tarkistus,syote,k1))
 	valmis.pack()
 	
