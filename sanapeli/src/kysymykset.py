c6c260b9be0f4467507d40eee33aeb4b7ac0a938
Siiha <90766306+Siiha@users.noreply.github.com>
2021-12-26 20:09:47 +0200

    Add files via upload

--- sanapeli/src/kysymykset.py
+++ sanapeli/src/kysymykset.py
@@ -5,24 +5,24 @@ con = sqlite3.connect('data.db')
 cur = con.cursor()
 def ekavika(x,ar):
 	if ar == 0:
-		v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like '"+x+"%'")]
+		v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like ?",(x+"%",))]
 		s="Sana jonka ensimmäinen kirjain on "+x
 	else:
-		v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like '%"+x+"'")]
+		v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like ?",("%"+x,))]
 		s="Sana jonka viimeinen kirjain on "+x
 	return v,s
 
 
 def pitka(x):
-	v = [i[0] for i in cur.execute("SELECT sana FROM sanat where len >= "+str(x))]
+	v = [i[0] for i in cur.execute("SELECT sana FROM sanat where len >= ?",(str(x),))]
 	s="Sana joka on vähintään "+str(x)+" merkkiä pitkä"
 	return v,s
 def sisaltaa(x):
-	v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like '%"+x+"%'")]
+	v = [i[0] for i in cur.execute("SELECT sana FROM sanat where sana like ?",("%"+x+"%",))]
 	s = "Sana joka sisältää kirjainyhdistelmän "+x
 	return v,s
 def sanoja(x):
-	v = [i[0] for i in cur.execute("Select sana from sanat where sanoja >= "+str(x))]
+	v = [i[0] for i in cur.execute("Select sana from sanat where sanoja >= ?",(str(x),))]
 	s = "Sana joka sisältää vähintään "+str(x)+" sanaa"
 	return v,s
 k = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, å, ä".replace(", ","")
